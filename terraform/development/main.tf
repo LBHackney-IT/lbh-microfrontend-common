28cd6b7415099bce081e59acfe918f565dd056a6
gabimartini <gabimartinii@hotmail.com>
2022-07-28 10:26:01 +0100

    add maa  cors rule

--- terraform/development/main.tf
+++ terraform/development/main.tf
@@ -23,29 +23,32 @@ resource "aws_s3_bucket" "frontend-bucket-development" {
   cors_rule {
     allowed_headers = ["*"]
     allowed_methods = ["GET"]
-    allowed_origins = ["https://manage-my-home-development.hackney.gov.uk"]
-    expose_headers  = ["x-amz-server-side-encryption","x-amz-request-id","x-amz-id-2"]
+    allowed_origins = [
+      "https://manage-my-home-development.hackney.gov.uk",
+      "https://manage-arrears-leasehold-development.hackney.gov.uk"
+    ]
+    expose_headers  = ["x-amz-server-side-encryption", "x-amz-request-id", "x-amz-id-2"]
     max_age_seconds = 3000
   }
 }
 module "cloudfront-development" {
-  source = "github.com/LBHackney-IT/aws-hackney-common-terraform.git//modules/cloudfront/s3_distribution"
-  s3_domain_name = aws_s3_bucket.frontend-bucket-development.bucket_regional_domain_name
-  origin_id = "mtfh-t-and-l-common-frontend"
-  s3_bucket_arn = aws_s3_bucket.frontend-bucket-development.arn
-  s3_bucket_id = aws_s3_bucket.frontend-bucket-development.id
+  source                       = "github.com/LBHackney-IT/aws-hackney-common-terraform.git//modules/cloudfront/s3_distribution"
+  s3_domain_name               = aws_s3_bucket.frontend-bucket-development.bucket_regional_domain_name
+  origin_id                    = "mtfh-t-and-l-common-frontend"
+  s3_bucket_arn                = aws_s3_bucket.frontend-bucket-development.arn
+  s3_bucket_id                 = aws_s3_bucket.frontend-bucket-development.id
   orginin_access_identity_desc = "T&L common frontend cloudfront identity"
-  cname_aliases = []
-  environment_name = "development"
-  cost_code = "B0811"
-  project_name = "MTFH Tenants and Leaseholders"
-  use_cloudfront_cert = true
-  compress = true
+  cname_aliases                = []
+  environment_name             = "development"
+  cost_code                    = "B0811"
+  project_name                 = "MTFH Tenants and Leaseholders"
+  use_cloudfront_cert          = true
+  compress                     = true
 }
 
 resource "aws_ssm_parameter" "cdn" {
-  name  = "/housing-tl/development/common-app-url"
-  type  = "String"
-  value = "https://${module.cloudfront-development.cloudfront_domain_name}"
+  name      = "/housing-tl/development/common-app-url"
+  type      = "String"
+  value     = "https://${module.cloudfront-development.cloudfront_domain_name}"
   overwrite = true
 }
